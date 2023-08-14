e6488123a8d66044f5b6385e0c3095cf4ceb373e
Antonov333 <antonovsergei333@yahoo.com>
2023-08-14 22:13:05 +0300

    Course3 Lesson 4 SQL Intro initial commit

---
+++ src/main/java/pro/sky/course3lesson4/Course3Lesson4Application.java
@@ -0,0 +1,13 @@
+package pro.sky.course3lesson4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course3Lesson4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Course3Lesson4Application.class, args);
+    }
+
+}
